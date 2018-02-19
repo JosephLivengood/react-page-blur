f01e3a199f68927fb4461ab380c4c9378a4b3ec9
Joseph Livengood <JFLivengood@gmail.com>
2018-02-18 19:03:40 -0500

    Basic package component

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+class PageBlur extends React.Component {
+  render() {
+    return (
+      <div className='page-blur'>
+        Hello
+      </div>
+    );
+  }
+}
+
+export default PageBlur;
