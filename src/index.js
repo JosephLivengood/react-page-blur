1a711375c6be82b52ad473d4e345a78e54505c01
Joseph Livengood <JFLivengood@gmail.com>
2018-02-20 19:55:14 -0500

    Implementation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,86 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './index.css';
 
 class PageBlur extends React.Component {
+  componentWillRecieveNewProps(nprops) {
+    if (nprops.active !== this.props.active) {
+      if (nprops.active) {
+        this.props.onActive();
+      } else {
+        this.props.offActive();
+      }
+    }
+  }
+  getClasses() {
+    let classes = [];
+    if (this.props.pulse) classes.push('pulse');
+    return classes.join(' ') + ' ' + this.props.className;
+  }
+  getStyle() {
+    return Object.assign(
+      {},
+      this.props.active && style.root,
+      {
+        opacity: this.props.opacity,
+        backgroundColor: this.props.color,
+        backgroundImage: this.props.image ? `url('${this.props.image}')` : 'none',
+        backgroundRepeat: this.props.imageRepeat,
+        backgroundPosition: this.props.imagePosition
+      }
+    );
+  }
   render() {
+    if (!this.props.active) return null;
+
     return (
-      <div className='page-blur'>
-        Hello
+      <div
+        className={this.getClasses()}
+        style={this.getStyle()}
+        onClick={this.props.onOffClick}
+      >
+        {this.props.children}
       </div>
     );
+  }
+}
+
+PageBlur.propsTypes = {
+  active: PropTypes.bool,
+  opacity: PropTypes.number,
+  color: PropTypes.string,
+  pulse: PropTypes.bool,
+  image: PropTypes.string,
+  imageRepeat: PropTypes.oneOf(['repeat', 'repeat-x', 'repeat-y', 'no-repeat']),
+  imagePosition: PropTypes.string,
+  className: PropTypes.string,
+  onOffClick: PropTypes.func,
+  onActive: PropTypes.func,
+  offActive: PropTypes.func
+};
+
+PageBlur.defaultProps = {
+  active: false,
+  opacity: 0.5,
+  color: 'black',
+  pulse: false,
+  image: undefined,
+  imageRepeat: 'repeat',
+  imagePosition: 'center',
+  className: '',
+  onOffClick: function(){},
+  onActive: function(){},
+  offActive: function(){}
+};
+
+const style = {
+  root: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 99999
   }
 }
 
